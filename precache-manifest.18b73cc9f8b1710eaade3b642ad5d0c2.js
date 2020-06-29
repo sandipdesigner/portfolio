a7fdf69c99302f53faefef5824fd657b18748f74
Sandip Chakraborty <sandip.chakraborty@ingrammicro.com>
2020-06-29 12:01:28 +0530

    Updates

--- precache-manifest.18b73cc9f8b1710eaade3b642ad5d0c2.js
+++ precache-manifest.18b73cc9f8b1710eaade3b642ad5d0c2.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6015a0083d7c3e349faf60d3189ca1b0",
+    "revision": "49df56363272272162668df513e2098f",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "8eed12a71b1a844816e4",
+    "revision": "f775fa109ed5ae318d23",
     "url": "/portfolio/static/css/main.5f361e03.chunk.css"
   },
   {
-    "revision": "bf79e0fc48d59fd198f7",
-    "url": "/portfolio/static/js/2.4c736102.chunk.js"
+    "revision": "97e63636bc8fb388b7b4",
+    "url": "/portfolio/static/js/2.ca42dc3b.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/portfolio/static/js/2.4c736102.chunk.js.LICENSE.txt"
+    "url": "/portfolio/static/js/2.ca42dc3b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8eed12a71b1a844816e4",
-    "url": "/portfolio/static/js/main.10a1b4a7.chunk.js"
+    "revision": "f775fa109ed5ae318d23",
+    "url": "/portfolio/static/js/main.b2a783b5.chunk.js"
   },
   {
     "revision": "7a6cc99bd8f08f9fa6ec",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.0da11d69fbca1a6be6b699b2717e1562.js"
+  "/portfolio/precache-manifest.18b73cc9f8b1710eaade3b642ad5d0c2.js"
 );
 
 self.addEventListener('message', (event) => {
