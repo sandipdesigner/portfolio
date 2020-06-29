edeceeca9e4264d34faa5470c805ac2c3991859a
Sandip Chakraborty <sandip.chakraborty@ingrammicro.com>
2020-06-29 21:36:11 +0530

    Updated navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import React from 'react';
-import './main.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './main.css';
 function App() {
   return (
     <div className="App">
       <nav>
+      <div className="container">
+      <div className="row">
+      <div className="col-xl-12">
+
+      <div className="d-flex-justified">
       <h1 id="logo">SC</h1>
+
+      <div className="navmenu">
+        
+      <div className="toggleMenu">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
       <ul>
         <li>Home</li>
         <li>About</li>
         <li>Portfolio</li>
         <li>Contact</li>
       </ul>
+     
+      </div>
+
+      </div>
+
+      </div>
+      </div>
+      </div>
       </nav>
 
     </div>
