3fe4acc1dbb63b4bc6de52f2381c9f106f5fcf94
Sandip Chakraborty <sandip.chakraborty@ingrammicro.com>
2020-07-01 21:46:07 +0530

    Updates

---
+++ precache-manifest.225a6075f3dff1c62a35e577b1b9900e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d31fd588129ec6ef3a16520c83fc33f7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "cc7f8db783d0a811e3fa",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3d2ad6eaf3073d33c2d0",
+    "url": "/portfolio/static/css/main.8f6eaf48.chunk.css"
+  },
+  {
+    "revision": "cc7f8db783d0a811e3fa",
+    "url": "/portfolio/static/js/2.c1fb9b87.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.c1fb9b87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d2ad6eaf3073d33c2d0",
+    "url": "/portfolio/static/js/main.e9ac3620.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.1da4ecc756784196dbf3dd49ef124f49.js"
+  "/portfolio/precache-manifest.225a6075f3dff1c62a35e577b1b9900e.js"
 );
 
 self.addEventListener('message', (event) => {
