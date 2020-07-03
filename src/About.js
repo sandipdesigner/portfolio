45d565a683afc5b5e28cab163b97febe8f20daab
Sandip Chakraborty <sandip.chakraborty@ingrammicro.com>
2020-07-04 00:56:35 +0530

    Updated about page

--- src/About.js
+++ src/About.js
@@ -1,11 +1,65 @@
 import React from 'react';
 import './main.css';
+import Pdf from './sandip chakraborty-cv.pdf'
+
 function About() {
   return (
       <header>
       <div className="container">
-      <h1>About page</h1>
-      <p className="mt-10">Content will be here soon....</p>
+      <div className="row">
+      <div className="col-xl-6 col-lg-6 col-md-6">
+      <h1 className="mb-20"><span>Introduction</span> </h1>
+      <p className="mt-10 mb-30">I have 7+ years of experience in the fields of UI/UX designing and Front end development. I also have excellent experience in managing clients and delivery.</p>
+     
+      <a href={Pdf} target="_blank" className="btn-purple">Download Resume</a>
+      </div>
+
+      <div className="col-xl-6 col-lg-6 col-md-6 skillsection">
+      
+      <p>HTML</p>
+      <div class="skills-container mb-15">
+        <div class="skills w-90">90%</div>
+        </div>
+
+        <p>CSS</p>
+        <div class="skills-container mb-15">
+        <div class="skills w-90">90%</div>
+        </div>
+
+        <p>JAVASCRIPT</p>
+        <div class="skills-container mb-15">
+        <div class="skills w-70">70%</div>
+        </div>
+
+        <p>JQUERY</p>
+        <div class="skills-container mb-15">
+        <div class="skills w-70">70%</div>
+        </div>
+
+        <p>React JS</p>
+        <div class="skills-container mb-15">
+        <div class="skills w-60">60%</div>
+        </div>
+
+        <p>Angular 2+</p>
+        <div class="skills-container mb-15">
+        <div class="skills w-40">40%</div>
+        </div>
+
+        <p>Adobe photoshop</p>
+        <div class="skills-container mb-15">
+        <div class="skills w-90">90%</div>
+        </div>
+
+        <p>Adobe illustrator</p>
+        <div class="skills-container mb-15">
+        <div class="skills w-80">80%</div>
+        </div>
+
+      </div>
+
+
+      </div>
       </div>
       </header>
   );
--- src/Home.js
+++ src/Home.js
@@ -9,6 +9,9 @@ function Home() {
     
       <header>
         <div className="container">
+          <div className="row">
+            <div className="col-xl-12">
+              
           <h1>Hello, I'm <span>Sandip Chakraborty</span><br/>
           I am an <Typical
         steps={['UI Developer.', 3000, 'UI Designer.', 3000 ]}
@@ -29,8 +32,11 @@ function Home() {
         </div>
 
         <Link to="/portfolio/about" className="btn-purple">Learn more</Link>
-          
+
         </div>
+        </div>
+        </div>
+
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
           <path fill="#ffffff" fill-opacity="0.3" d="M0,192L60,197.3C120,203,240,213,360,202.7C480,192,600,160,720,170.7C840,181,960,235,1080,256C1200,277,1320,267,1380,261.3L1440,256L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
         </svg>
