9c8599124e7c43fa3ae4bbb51cbd2390a63c12e3
Sandip Chakraborty <sandip.chakraborty@ingrammicro.com>
2020-07-04 01:29:29 +0530

    Updated Route

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { BrowserRouter as Router , Switch , Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/portfolio">
     <div className="App">
       <Nav />
 
