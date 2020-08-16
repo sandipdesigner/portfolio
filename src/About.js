93da21f6fddb79a0df82980d5259edde9192eb8c
Sandip Chakraborty <sandip.frontend@outlook.com>
2020-08-16 18:51:00 +0530

    Updated Skills section in about

--- src/About.js
+++ src/About.js
@@ -7,58 +7,106 @@ function About() {
       <header>
       <div className="container">
       <div className="row">
-      <div className="col-xl-6 col-lg-6 col-md-6">
+      <div className="col-xl-5 col-lg-6">
       <h1 className="mb-20"><span>Introduction</span> </h1>
       <p className="mt-10 mb-30">I have 7+ years of experience in the fields of UI/UX designing and Front end development. I also have excellent experience in managing clients and delivery.</p>
      
       <a href={Pdf} target="_blank" className="btn-purple">Download Resume</a>
       </div>
 
-      <div className="col-xl-6 col-lg-6 col-md-6 skillsection">
+      <div className="col-xl-7 col-lg-6 skillsection">
+      <div className="row">
       
+      <div className="col-xl-4 col-lg-6 col-md-4">
       <p>HTML</p>
-      <div class="skills-container mb-15">
-        <div class="skills w-90">90%</div>
+      <div className="skills-container mb-15">
+        <div className="skills h-90"></div>
+        <div className="show-width"> 90 <span>%</span> </div>
+        </div>
         </div>
 
+        <div className="col-xl-4 col-lg-6 col-md-4">
         <p>CSS</p>
-        <div class="skills-container mb-15">
-        <div class="skills w-90">90%</div>
+        <div className="skills-container mb-15">
+        <div className="skills h-90"></div>
+        <div className="show-width"> 90 <span>%</span></div>
+        </div>
         </div>
 
-        <p>Responsive Design</p>
-        <div class="skills-container mb-15">
-        <div class="skills w-90">90%</div>
+         
+      <div className="col-xl-4 col-lg-6 col-md-4">
+      <p>Responsive Design</p>
+      <div className="skills-container mb-15">
+        <div className="skills h-90"></div>
+        <div className="show-width"> 90 <span>%</span> </div>
+        </div>
         </div>
 
-        <p>JAVASCRIPT</p>
-        <div class="skills-container mb-15">
-        <div class="skills w-70">70%</div>
+        <div className="col-xl-4 col-lg-6 col-md-4">
+      <p>Javascript</p>
+      <div className="skills-container mb-15">
+        <div className="skills h-80"></div>
+        <div className="show-width"> 80 <span>%</span> </div>
+        </div>
         </div>
 
-        <p>JQUERY</p>
-        <div class="skills-container mb-15">
-        <div class="skills w-70">70%</div>
+        <div className="col-xl-4 col-lg-6 col-md-4">
+      <p>jquery</p>
+      <div className="skills-container mb-15">
+        <div className="skills h-80"></div>
+        <div className="show-width"> 80 <span>%</span> </div>
+        </div>
         </div>
 
-        <p>React JS</p>
-        <div class="skills-container mb-15">
-        <div class="skills w-60">60%</div>
+
+        <div className="col-xl-4 col-lg-6 col-md-4">
+      <p>React Js</p>
+      <div className="skills-container mb-15">
+        <div className="skills h-80"></div>
+        <div className="show-width"> 70 <span>%</span> </div>
+        </div>
         </div>
 
-        <p>Angular 2+</p>
-        <div class="skills-container mb-15">
-        <div class="skills w-40">40%</div>
+        <div className="col-xl-4 col-lg-6 col-md-4">
+      <p>Angular JS 2+</p>
+      <div className="skills-container mb-15">
+        <div className="skills h-50"></div>
+        <div className="show-width"> 50 <span>%</span> </div>
+        </div>
         </div>
 
-        <p>Adobe photoshop</p>
-        <div class="skills-container mb-15">
-        <div class="skills w-90">90%</div>
+        <div className="col-xl-4 col-lg-6 col-md-4">
+      <p>Adobe Photoshop</p>
+      <div className="skills-container mb-15">
+        <div className="skills h-90"></div>
+        <div className="show-width"> 90 <span>%</span> </div>
+        </div>
         </div>
 
-        <p>Adobe illustrator</p>
-        <div class="skills-container mb-15">
-        <div class="skills w-80">80%</div>
+        <div className="col-xl-4 col-lg-6 col-md-4">
+      <p>Adobe Illustrator</p>
+      <div className="skills-container mb-15">
+        <div className="skills h-80"></div>
+        <div className="show-width"> 80 <span>%</span> </div>
+        </div>
+        </div>
+
+        <div className="col-xl-4 col-lg-6 col-md-4">
+      <p>Adobe XD</p>
+      <div className="skills-container mb-15">
+        <div className="skills h-70"></div>
+        <div className="show-width"> 70 <span>%</span> </div>
+        </div>
+        </div>
+
+        <div className="col-xl-4 col-lg-6 col-md-4">
+      <p>Figma</p>
+      <div className="skills-container mb-15">
+        <div className="skills h-70"></div>
+        <div className="show-width"> 70 <span>%</span> </div>
+        </div>
+        </div>
+
         </div>
 
       </div>
