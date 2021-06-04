2b2b39064adf2e1924764f777a3b44c0952ef471
Sandip Chakraborty <sandip.chakraborty88@gmail.com>
2021-06-04 19:54:41 +0530

    Updates

--- precache-manifest.4748856c519b97ae858f41f42a863bd6.js
+++ precache-manifest.4748856c519b97ae858f41f42a863bd6.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cc084f3d8aecb4d3eb09c89e1d980fed",
+    "revision": "43b0d6d9de84cb502883826ef9759f58",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "d228af68a0b20fea6677",
+    "revision": "22e1181fe7a53ef30805",
     "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "45a44d3e37b0db2e367d",
+    "revision": "279e18d297a582c71c5b",
     "url": "/portfolio/static/css/main.d8fb7c2d.chunk.css"
   },
   {
-    "revision": "d228af68a0b20fea6677",
-    "url": "/portfolio/static/js/2.fa3aeb22.chunk.js"
+    "revision": "22e1181fe7a53ef30805",
+    "url": "/portfolio/static/js/2.d4eda64b.chunk.js"
   },
   {
     "revision": "eb79dc9b9988b5c922266f4e093e32ca",
-    "url": "/portfolio/static/js/2.fa3aeb22.chunk.js.LICENSE.txt"
+    "url": "/portfolio/static/js/2.d4eda64b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "45a44d3e37b0db2e367d",
-    "url": "/portfolio/static/js/main.c2ddd501.chunk.js"
+    "revision": "279e18d297a582c71c5b",
+    "url": "/portfolio/static/js/main.4f20d13b.chunk.js"
   },
   {
     "revision": "7a6cc99bd8f08f9fa6ec",
